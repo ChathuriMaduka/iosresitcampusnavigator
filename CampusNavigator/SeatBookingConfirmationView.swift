cc8076285e69070cc6b3698b80a1afc430af9a7f
ChathuriMaduka <cmadu.96@gmail.com>
2025-06-06 14:25:13 +0530

    ui changes

---
+++ CampusNavigator/SeatBookingConfirmationView.swift
@@ -0,0 +1,18 @@
+//
+//  SeatBookingConfirmationView.swift
+//  CampusNavigator
+//
+//  Created by Chathuri Maduka on 2025-06-06.
+//
+
+import SwiftUI
+
+struct SeatBookingConfirmationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SeatBookingConfirmationView()
+}
