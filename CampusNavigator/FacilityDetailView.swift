0470f0ed6a60adba1d7c3e96514cf2834088fc9d
malsha7 <malsha74bopage@gmail.com>
2025-06-06 15:06:30 +0530

    Create FacilityDetailView

---
+++ CampusNavigator/FacilityDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  FacilityDetailView.swift
+//  CampusNavigator
+//
+//  Created by Malsha Bopage on 2025-06-06.
+//
+
+import SwiftUI
+
+struct FacilityDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FacilityDetailView()
+}
