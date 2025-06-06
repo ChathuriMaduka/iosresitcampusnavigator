a1239f8c733ce6f573932537615b5ac9ae468bbd
malsha7 <malsha74bopage@gmail.com>
2025-06-07 00:07:10 +0530

    Created ProfileDetailsView UI

---
+++ CampusNavigator/User Profile/ProfileDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileDetailsView.swift
+//  CampusNavigator
+//
+//  Created by Malsha Bopage on 2025-06-07.
+//
+
+import SwiftUI
+
+struct ProfileDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileDetailsView()
+}
