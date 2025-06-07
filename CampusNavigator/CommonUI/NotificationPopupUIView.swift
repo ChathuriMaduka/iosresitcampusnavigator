76fd02ff1cfa54a23eb26d0a1f37f0b67b75346d
Tharangani Hagoda Arachchi <tharangijayamuthu@gmail.com>
2025-06-08 00:10:16 +0530

    notification popup view

---
+++ CampusNavigator/CommonUI/NotificationPopupUIView.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationPopupUIView.swift
+//  CampusNavigator
+//
+//  Created by Tharangani Hagoda Arachchi on 07/06/2025.
+//
+
+import SwiftUI
+
+struct NotificationPopupUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationPopupUIView()
+}
