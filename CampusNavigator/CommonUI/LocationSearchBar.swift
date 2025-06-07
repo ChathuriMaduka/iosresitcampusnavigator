bbaa43ef8f75f5d87dd61ce56c19fe0234104512
malsha7 <malsha74bopage@gmail.com>
2025-06-07 22:56:17 +0530

    Created HomeScreen UI

---
+++ CampusNavigator/CommonUI/LocationSearchBar.swift
@@ -0,0 +1,18 @@
+//
+//  LocationSearchBar.swift
+//  CampusNavigator
+//
+//  Created by Malsha Bopage on 2025-06-07.
+//
+
+import SwiftUI
+
+struct LocationSearchBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationSearchBar()
+}
