2ff38815a916918ffb39c63345ced74a55365f61
Tharangani Hagoda Arachchi <tharangijayamuthu@gmail.com>
2025-06-07 14:39:33 +0530

    create facility notificaton view

---
+++ CampusNavigator/CommonUI/FacilityCardUIView.swift
@@ -0,0 +1,18 @@
+//
+//  FacilityCardUIView.swift
+//  CampusNavigator
+//
+//  Created by Tharangani Hagoda Arachchi on 07/06/2025.
+//
+
+import SwiftUI
+
+struct FacilityCardUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FacilityCardUIView()
+}
---
+++ CampusNavigator/Facilities /FacilityUIView.swift
@@ -0,0 +1,18 @@
+//
+//  FacilityUIView.swift
+//  CampusNavigator
+//
+//  Created by Tharangani Hagoda Arachchi on 07/06/2025.
+//
+
+import SwiftUI
+
+struct FacilityUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FacilityUIView()
+}
