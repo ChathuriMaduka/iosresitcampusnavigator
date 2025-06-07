fb969affeb768344e1458a43ec91c8711eaad80b
Tharangani Hagoda Arachchi <tharangijayamuthu@gmail.com>
2025-06-07 20:24:28 +0530

    library seat booking view

---
+++ CampusNavigator/Facilities /LibraryBookingUIView.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryBookingUIView.swift
+//  CampusNavigator
+//
+//  Created by Tharangani Hagoda Arachchi on 07/06/2025.
+//
+
+import SwiftUI
+
+struct LibraryBookingUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LibraryBookingUIView()
+}
