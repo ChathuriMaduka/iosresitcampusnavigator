f523ba49ac3da482a7b38c8068365db678632440
ChathuriMaduka <cmadu.96@gmail.com>
2025-06-07 14:31:52 +0530

    Created Library and Cafetaria Views

---
+++ CampusNavigator/Facilities /CafeteriaView.swift
@@ -0,0 +1,18 @@
+//
+//  CafeteriaView.swift
+//  CampusNavigator
+//
+//  Created by Chathuri Maduka on 2025-06-07.
+//
+
+import SwiftUI
+
+struct CafeteriaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CafeteriaView()
+}
